e3b115339f8027a91537b7ea1c9383c79d6a89eb
72394_Rani <ranirangari2@gmail.com>
2023-08-15 06:26:00 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Footer from "./student/components/Footer/Footer";
+import Navigation from "./student/components/Navigation/Navigation";
+import HomePage from "./student/pages/HomePage/HomePage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      <div>
+        <HomePage />
+      </div>
+      <Footer/>
     </div>
   );
 }
