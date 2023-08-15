edf5982e8eb4ae0a70b9aa2da99dcc7b38239f82
RANI RANGARI <108244843+ranirangari@users.noreply.github.com>
2023-08-15 05:56:10 +0530

    Add files via upload

---
+++ data/singlebed.js
@@ -0,0 +1,226 @@
+export const singlebed = [
+
+  {
+    imageUrl:
+      "https://images.unsplash.com/photo-1560067174-c5a3a8f37060?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzZ8fHNpbmdsZSUyMGJlZHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+      
+        id: 1,
+        type: 'House',
+      
+        description:
+          'This stunning modern house is situated in a serene suburban neighborhood, offering a perfect blend of contemporary design and comfortable living. The exterior features clean lines, large windows, and a minimalist aesthetic, creating a striking and sophisticated appeal.',
+        // image: House1,
+        // imageLg: House1Lg,
+        country: 'Nagpur',
+        address: '724 Sarojini Nagar Market',
+        wifi: 'Wifi Available',
+        food: 'Food Available',
+        bed: 'Single Bed',
+        year: '2016',
+        price: '₹4000',
+        agent: {
+          // image: Agent1,
+          name: 'Shreya Sharma',
+          phone: '0123 456 78910',
+        },
+      },
+      {
+        imageUrl:"https://images.unsplash.com/photo-1512918580421-b2feee3b85a6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDF8fHNpbmdsZSUyMGJlZHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        id: 2,
+        type: 'House',
+
+        description:
+          'As you approach the house, you are greeted by a beautifully landscaped front yard, complete with manicured gardens and a paved driveway leading to the attached garage. The entrance is adorned with a sleek wooden door, welcoming you into a world of modern elegance.',
+        // image: House2,
+        // imageLg: House2Lg,
+        country: 'Pune',
+        address: '798 Hiranandani Gardens NJ 08807',
+        wifi: 'Wifi Available',
+        food: 'Food Not Available',
+        bed: 'Single Bed',
+        year: '2016',
+        price: '₹2000',
+        agent: {
+          // image: Agent2,
+          name: 'Ajay Verma',
+          phone: '0123 456 78910',
+        },
+      },
+      {
+        imageUrl:"https://images.unsplash.com/photo-1657040899594-34f4b6739988?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NzZ8fHNpbmdsZSUyMGJlZHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        id: 3,
+        type: 'House',
+     
+        description:
+          'Outside, you will find a beautifully landscaped backyard with a patio area ideal for outdoor dining and entertaining. The yard is spacious enough for various activities and offers privacy, thanks to well-placed fencing and lush greenery.',
+        // image: House3,
+        // imageLg: House3Lg,
+        country: 'Pune',
+        address: '2 Jubilee Hills , VA 22304',
+        wifi: 'Wifi Available',
+        food: 'Food Not Available',
+        bed: 'Double Bed',
+        year: '2016',
+        price: '₹3000',
+        agent: {
+          // image: Agent3,
+          name: 'Akash Jha',
+          phone: '0123 456 78910',
+        },
+      },
+      {
+        imageUrl:"https://images.unsplash.com/photo-1505693314120-0d443867891c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8c2luZ2xlJTIwYmVkfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+        id: 4,
+        type: 'House',
+    
+        description:
+          'As for amenities, this house is conveniently located near several amenities. Within a short distance, you will find shopping centers, restaurants, parks, and recreational facilities. The neighborhood also provides easy access to schools, medical facilities, and major transportation routes, making daily commutes a breeze.',
+        // image: House4,
+        // imageLg: House4Lg,
+        country: 'Pune',
+        address: '84 Park Street , FL 32927',
+        wifi: 'Wifi Available',
+        food: 'Food Available',
+        bed: 'Double Bed',
+        year: '2016',
+        price: '₹5000',
+        agent: {
+          // image: Agent4,
+          name: 'Saniya Iyer',
+          phone: '0123 456 78910',
+        },
+      },
+      {
+        imageUrl:"https://images.unsplash.com/photo-1560067174-c5a3a8f37060?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzZ8fHNpbmdsZSUyMGJlZHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        id: 5,
+        type: 'House',
+     
+        description:
+          'This stunning modern house is situated in a serene suburban neighborhood, offering a perfect blend of contemporary design and comfortable living. The exterior features clean lines, large windows, and a minimalist aesthetic, creating a striking and sophisticated appeal.',
+        // image: House5,
+        // imageLg: House5Lg,
+        country: 'Mumbai',
+        address: '28 MG Road PA 18974',
+        wifi: 'Wifi Available',
+        food: 'Food Not Available',
+        bed: 'Single Bed',
+        year: '2016',
+        price: '₹2000',
+        agent: {
+          // image: Agent5,
+          name: 'Palash Jain',
+          phone: '0123 456 78910',
+        },
+      },
+      {
+        imageUrl:"https://images.unsplash.com/photo-1564273795917-fe399b763988?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2luZ2xlJTIwYmVkfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+        id: 6,
+        type: 'House',
+     
+        description:
+          'As you approach the house, you are greeted by a beautifully landscaped front yard, complete with manicured gardens and a paved driveway leading to the attached garage. The entrance is adorned with a sleek wooden door, welcoming you into a world of modern elegance.',
+        // image: House6,
+        // imageLg: House6Lg,
+        country: 'Mumbai',
+        address: '32 Pawnee Street , MT 59701',
+        wifi: 'Wifi Available',
+        food: 'Food Not Available',
+        bed: 'Double Bed',
+        year: '2016',
+        price: '₹3000',
+        agent: {
+          // image: Agent6,
+          name: 'Siya Patil',
+          phone: '0123 456 78910',
+        },
+      },
+      {
+        imageUrl:"https://images.unsplash.com/photo-1619011502686-b512e3989a33?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c2luZ2xlJTIwYmVkfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+        id: 7,
+        type: 'House',
+     
+        description:
+          'As you approach the house, you are greeted by a beautifully landscaped front yard, complete with manicured gardens and a paved driveway leading to the attached garage. The entrance is adorned with a sleek wooden door, welcoming you into a world of modern elegance.',
+        // image: House6,
+        // imageLg: House6Lg,
+        country: 'Mumbai',
+        address: '32 Pawnee Street , MT 59701',
+        wifi: 'Wifi Available',
+        food: 'Food Not Available',
+        bed: 'Double Bed',
+        year: '2016',
+        price: '₹3000',
+        agent: {
+          // image: Agent6,
+          name: 'Siya Patil',
+          phone: '0123 456 78910',
+        },
+      },
+      {
+        imageUrl:"https://images.unsplash.com/photo-1585314545477-c4be889f112c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8c2luZ2xlJTIwYmVkfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+        id: 8,
+        type: 'House',
+     
+        description:
+          'As you approach the house, you are greeted by a beautifully landscaped front yard, complete with manicured gardens and a paved driveway leading to the attached garage. The entrance is adorned with a sleek wooden door, welcoming you into a world of modern elegance.',
+        // image: House6,
+        // imageLg: House6Lg,
+        country: 'Mumbai',
+        address: '32 Pawnee Street , MT 59701',
+        wifi: 'Wifi Available',
+        food: 'Food Not Available',
+        bed: 'Double Bed',
+        year: '2016',
+        price: '₹3000',
+        agent: {
+          // image: Agent6,
+          name: 'Siya Patil',
+          phone: '0123 456 78910',
+        },
+      },
+      {
+        imageUrl:"https://images.unsplash.com/flagged/photo-1556438758-8d49568ce18e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHNpbmdsZSUyMGJlZHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        id: 9,
+        type: 'House',
+     
+        description:
+          'As you approach the house, you are greeted by a beautifully landscaped front yard, complete with manicured gardens and a paved driveway leading to the attached garage. The entrance is adorned with a sleek wooden door, welcoming you into a world of modern elegance.',
+        // image: House6,
+        // imageLg: House6Lg,
+        country: 'Mumbai',
+        address: '32 Pawnee Street , MT 59701',
+        wifi: 'Wifi Available',
+        food: 'Food Not Available',
+        bed: 'Double Bed',
+        year: '2016',
+        price: '₹3000',
+        agent: {
+          // image: Agent6,
+          name: 'Siya Patil',
+          phone: '0123 456 78910',
+        },
+      },
+      {
+        imageUrl:"https://images.unsplash.com/photo-1532344214108-1b6d425db572?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjN8fHNpbmdsZSUyMGJlZHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        id: 10,
+        type: 'House',
+     
+        description:
+          'As you approach the house, you are greeted by a beautifully landscaped front yard, complete with manicured gardens and a paved driveway leading to the attached garage. The entrance is adorned with a sleek wooden door, welcoming you into a world of modern elegance.',
+        // image: House6,
+        // imageLg: House6Lg,
+        country: 'Mumbai',
+        address: '32 Pawnee Street , MT 59701',
+        wifi: 'Wifi Available',
+        food: 'Food Not Available',
+        bed: 'Double Bed',
+        year: '2016',
+        price: '₹3000',
+        agent: {
+          // image: Agent6,
+          name: 'Siya Patil',
+          phone: '0123 456 78910',
+        },
+      },
+  
+];
